cc608bb4dd3a43909c8a9f01760103905afdb86e
Radhe0007 <rajprince5670@gmail.com>
2024-12-31 15:58:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "7839106251"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yourtoofan/CHATBOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Radhe0007/CHATBOT-radhe")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 SUPPORT_GRP = "FRIENDSHUBCHATZONE"
 UPDATE_CHNL = "ll_BOTCHAMBER_ll"
-OWNER_USERNAME = "ll_SAN4TANI_ll"
+OWNER_USERNAME = "ll_RADHE7_ll"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
